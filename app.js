3405a22e0f7c7f7397e47a40d17414aa0ff25574
Anit <anit7795@gmail.com>
2025-06-09 01:24:16 +0530

    while loop to loop prompt and checking guess

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 let maximum = parseInt(prompt('Enter Your Maximum Number: '));
-
 while(!maximum) {
-    maximum = (prompt('Enter Valid Number!: '));
+    maximum = parseInt(prompt('Enter Valid Number!: '));
 }
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 
-console.log(targetNum)
+console.log(targetNum);
+
+let guess = prompt('Enter Your First Guess: ');
+
+while (guess !== targetNum) {
+    if(guess > targetNum) {
+        guess = parseInt(prompt('Too high, Enter a new Guess.'))
+    } else {
+        guess = parseInt(prompt("Too low, Try again"))
+    }
+}
+
+console.log('You Got it')
