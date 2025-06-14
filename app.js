acb41fed01e2d74e802ef5106459c15c7c14191d
Anit <anit7795@gmail.com>
2025-06-14 21:24:49 +0530

    Bug fixes

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
 let attempts = 1;
 
-let guess = prompt('Enter Your First Guess: ');
+let guess = prompt('Enter Your First Guess: (type "q" to quit the game)');
 
 while (parseInt(guess) !== targetNum) {
-    if(guess === 'quit') break;
-    attempts++;
+    if(guess === 'q') break;
+    guess = parseInt(guess);
     if(guess > targetNum) {
         guess = prompt('Too high, Enter a new Guess.');
-    } else {
+        attempts++;
+    } else if (guess < targetNum) {
         guess = prompt("Too low, Try again");
+        attempts++;
+    } else {
+        guess = prompt('Invalid guess. enter a number or "q" to exit')
     }
 }
 
-if(guess === 'quit') {
+if(guess === 'q') {
     console.log('You quit')
 } else {
     console.log('Congrats, You win!')
